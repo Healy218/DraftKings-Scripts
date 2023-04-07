c868beef7d3516859aee3bfe8f9e1242e46dee29
Matt Healy <mrhealy115@gmail.com>
2023-04-07 14:25:45 -0500

    MLB data

--- Best Team Mode/MLBBestTeam.py
+++ Best Team Mode/MLBBestTeam.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pulp import LpVariable, LpProblem, LpMaximize, LpInteger, lpSum, LpStatus
 
 # read in the csv file
-df = pd.read_csv("../DraftKings Scripts and Stats/MLB Stats/MLBgame1.csv")
+df = pd.read_csv("../DraftKings Scripts and Stats/MLB Stats/MLBgame2.csv")
 
 # create a LP problem
 prob = LpProblem("Fantasy Baseball Team Selector", LpMaximize)
