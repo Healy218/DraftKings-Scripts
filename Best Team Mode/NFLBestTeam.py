872e6eff1cb71b49b0a0294e99c3d97d80e2e4f3
Matt Healy <mrhealy115@gmail.com>
2023-08-29 07:13:32 -0500

    updated data and others

--- Best Team Mode/NFLBestTeam.py
+++ Best Team Mode/NFLBestTeam.py
@@ -3,7 +3,7 @@ import pulp as pl
 from pulp import LpVariable, LpProblem, LpMaximize, LpInteger, LpStatus
 
 # read in the csv file
-df = pd.read_csv("../DraftKings Scripts and Stats/players.csv")
+df = pd.read_csv("../DraftKings Scripts and Stats/NFL Stats/NFLweek1.csv")
 
 # create a LP problem
 prob = LpProblem("Fantasy Football Team Selector", LpMaximize)
@@ -14,29 +14,34 @@ budget = 50000
 
 # create a binary variable for each player
 player_vars = {(row.player, row.position): LpVariable(f"{row.player}_{row.position}", 0, 1, LpInteger)
-              for i, row in df.iterrows()}
+               for i, row in df.iterrows()}
 
 # add constraints for each position
 for pos, count in positions.items():
-    prob += sum(player_vars[player, pos] for player, p in player_vars.keys() if p == pos) == count
+    prob += sum(player_vars[player, pos]
+                for player, p in player_vars.keys() if p == pos) == count
 
 # add constraint to ensure that the selected FLEX isn't already on the team as a RB, WR or TE
 # add constraint to ensure that the selected FLEX isn't already on the team as a RB, WR or TE
-rb_wr_te_vars = [player_vars[player, pos] for player, pos in player_vars.keys() if pos in ["RB", "WR", "TE"]]
-flex_vars = [player_vars[player, pos] for player, pos in player_vars.keys() if pos == "FLEX"]
+rb_wr_te_vars = [player_vars[player, pos]
+                 for player, pos in player_vars.keys() if pos in ["RB", "WR", "TE"]]
+flex_vars = [player_vars[player, pos]
+             for player, pos in player_vars.keys() if pos == "FLEX"]
 for player, pos in player_vars.keys():
     if pos in ["RB", "WR", "TE"]:
         prob += player_vars[player, "FLEX"] <= 1 - player_vars[player, pos]
 
 
 # add budget constraint
-prob += sum(player_vars[player_name, pos] * df.loc[df["player"] == player_name, "cost"].values[0] for player_name, pos in player_vars.keys()) <= budget
+prob += sum(player_vars[player_name, pos] * df.loc[df["player"] == player_name,
+            "cost"].values[0] for player_name, pos in player_vars.keys()) <= budget
 
 # set objective function
-prob += sum(player_vars[player, pos] * df.loc[df["player"] == player, "point_value"].values[0] for player, pos in player_vars.keys())
+prob += sum(player_vars[player, pos] * df.loc[df["player"] == player,
+            "point_value"].values[0] for player, pos in player_vars.keys())
 
 # specify the CPLEX solver
-#prob.solve(solver=pl.getSolver('CPLEX_CMD'))
+# prob.solve(solver=pl.getSolver('CPLEX_CMD'))
 
 # solve the LP problem
 status = prob.solve(solver=pl.GLPK())
@@ -48,6 +53,7 @@ if status == 1:
     print("Best Team:")
     for player, pos in player_vars.keys():
         if player_vars[player, pos].varValue == 1.0:
-            print(f"{player} - {pos} - ${df.loc[df['player'] == player, 'cost'].values[0]} - {df.loc[df['player'] == player, 'point_value'].values[0]} pts")
+            print(
+                f"{player} - {pos} - ${df.loc[df['player'] == player, 'cost'].values[0]} - {df.loc[df['player'] == player, 'point_value'].values[0]} pts")
 else:
     print("The LP problem is not solved")
